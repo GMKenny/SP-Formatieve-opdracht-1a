3db5cce602a74a39abcc56e34dc1287599304a39
GMKenny <kenny.vandenberg@student.hu.nl>
2021-02-02 19:20:43 +0100

    Update

--- formatieve opdracht 1.py
+++ formatieve opdracht 1.py
@@ -1,42 +1,32 @@
 num = int(input("Hoe groot moet de piramide?"))
 
 # Opdracht 1
-def print_pyramid_up(index):
-    for index in range(index):
-        for item in range(index + 1):
-            print("*", end="")
-        print("")
-    print_pyramid_down(index - 1)
+def print_pyramid(index):
+    # Voor elk nummer in de range van 1 tot index
+    for num in range(1, index):
+        # Print "*" keer het huidige nummer
+        print("*" * num)
+    # Voor elk nummer vanaf index tot 0
+    for num in range(index, 0, -1):
+        # Print "*" keer het huidige nummer
+        print("*" * num)
 
 
-def print_pyramid_down(index):
-    for index in range(index, 0, -1):
-        for item in range(index, 0, -1):
-            print("*", end="")
-        print("")
+def print_pyramid_while(index):
+    counter = 0
+    # Zolang counter niet gelijk is aan de gegeven index
+    while counter != index:
+        # Counter is counter plus 1
+        counter += 1
+        # Print "*" keer de counter
+        print("*" * counter)
+    counter = index - 1
+    # Zolang counter niet gelijk is aan 0
+    while counter != 0:
+        # Print "*" keer de counter
+        print("*" * counter)
+        counter -= 1
 
 
-def print_pyramid_up_while(index):
-    num = 1
-    while num <= index:
-        num2 = 1
-        while num2 <= num:
-            print("*", end="")
-            num2 += 1
-        print("")
-        num += 1
-    print_pyramid_down_while(index - 1)
-
-
-def print_pyramid_down_while(index):
-    num = index
-    while num > 0:
-        num2 = num
-        while num2 > 0:
-            num2 -= 1
-            print("*", end="")
-        print("")
-        num -= 1
-
-print_pyramid_up(num)
-print_pyramid_up_while(num)
+print_pyramid(num)
+print_pyramid_while(num)
--- formatieve opdracht 9.py
+++ formatieve opdracht 9.py
@@ -3,7 +3,8 @@ def cyclisch(char, num):
     # Verandert de string in binary
     bin_code = "".join(format(ord(item), "b") for item in char)
     # Return bin_code van af num tot einde plus bin_code van het begin tot num
-    return bin_code[num:] + bin_code[:num]
+    return chr(int((bin_code[num:] + bin_code[:num]), 2))
 
 print(cyclisch("x", 3))
 print(cyclisch("x", -3))
+
